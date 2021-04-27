450c8483162f32e3ea032fb62017b2d8fb28e270
tzq0301 <7947437+tzq0301@user.noreply.gitee.com>
2021-04-27 15:04:09 +0800

    Initial commit

---
+++ src/main/java/cn/tzq0301/pos/PosApplication.java
@@ -0,0 +1,13 @@
+package cn.tzq0301.pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/tzq0301/pos/PosApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tzq0301.pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
